7c562c056317c6cf7368e28b889509d8ca9e78ad
Mauricio Castillo <mauricio.castillo@zeke.cl>
2019-12-29 23:58:59 -0300

    avances cuda

--- gso/refactor/repairTest2.py
+++ gso/refactor/repairTest2.py
@@ -1,3 +1,4 @@
+import numba
 from numba import cuda, float32
 import numpy as np
 import math
@@ -6,6 +7,11 @@ from datetime import datetime
 
 from problemas.knapsack.knapsack import KP
 TPB = 2
+BD = 1
+problema = KP(f'problemas/knapsack/instances/knapPI_1_500_1000_1')
+NUM_DIM = problema.getNumDim()
+NUM_FILAS = 50
+CAPACIDAD = problema.instance.capacidad
 
 @cuda.reduce
 def sum_reduce(a, b):
@@ -13,22 +19,9 @@ def sum_reduce(a, b):
 
 @cuda.jit
 def hacerCero(matrizSols, itemsEliminar, res):
-    solucionesc = cuda.shared.array(shape=(cuda.blockDim.x,matrizSols.shape[1]), dtype=float32)
-    pondc = cuda.shared.array(shape=pond.shape, dtype=float32)
-    resc = cuda.shared.array(shape=(cuda.blockDim.x, 1))
-
-    posGx, posGy = cuda.grid(2)
-    tx = cuda.threadIdx.x
-    ty = cuda.threadIdx.y
-
-    if posGx > res.shape[0] or posGy > matrizSols.shape[1]:
-        return
-
-@cuda.jit
-def cumple(matrizSols, capacidad, res):
-    solucionesc = cuda.shared.array(shape=(cuda.blockDim.x,matrizSols.shape[1]), dtype=float32)
-    capacidadc = cuda.shared.array(shape=capacidad.shape, dtype=float32)
-    resc = cuda.shared.array(shape=(cuda.blockDim.x, 1), dtype=float32)
+    solucionesc = cuda.shared.array(shape=(BD,NUM_DIM), dtype=float32)
+    itemsEliminarc = cuda.shared.array(shape=BD, dtype=float32)
+#    resc = cuda.shared.array(shape=(cuda.blockDim.x, 1))
 
     posGx, posGy = cuda.grid(2)
     tx = cuda.threadIdx.x
@@ -37,34 +30,65 @@ def cumple(matrizSols, capacidad, res):
     if posGx > res.shape[0] or posGy > matrizSols.shape[1]:
         return
     
-    bloqueX = cuda.blockIdx.x
-    dimBloqueX = cuda.blockDim.x
+    solucionesc[tx,ty] = matrizSols[posGx,posGy]
+    itemsEliminarc[tx] = itemsEliminar[posGx]
+    
+    cuda.syncthreads()
+    
+    solucionesc[tx,itemsEliminarc[ty]] = 0
+    res[posGx,posGy] = solucionesc[tx,ty]
+    
+    
+
+@cuda.jit
+def cumple(matrizSols, capacidad, res):
+    
+    solucionesc = cuda.shared.array(shape=(BD,NUM_DIM), dtype=float32)
+    resc = cuda.shared.array(shape=(BD,), dtype=float32)
+#    tmp = cuda.shared.array(shape=(BD, 1), dtype=float32)
+
+    posGx, posGy = cuda.grid(2)
+    tx = cuda.threadIdx.x
+    ty = cuda.threadIdx.y
+
+    if posGx > res.shape[0] or posGy > matrizSols.shape[1]:
+        return
+    
+#    bloqueX = cuda.blockIdx.x
+#    dimBloqueX = cuda.blockDim.x
 
     solucionesc[tx,ty]= matrizSols[posGx,posGy]
-    capacidadc[0] = capacidad[0]
+#    capacidadc[tx] = capacidad[0]
+#    print(capacidad[0])
+    resc[tx] = 0
 
     cuda.syncthreads()
 
     #expect = solucionesc.sum()      # numpy sum reduction
-    resc[tx] = sum_reduce(solucionesc[tx])   # cuda sum reduction
+#    print(resc[tx])
+#    print(solucionesc[tx,ty])
+    resc[tx] = resc[tx] + solucionesc[tx,ty]   # cuda sum reduction
+    cuda.syncthreads()
+#    resc[tx] = resc[tx] <= capacidadc[0]
     #assert expect == got
     #if ty == 0:
     #    tmp = 0
     #    for i in range(cuda.blockDim.y):
     #        resc[tx] += solucionesc[tx,i]
-    cuda.syncthreads()
+#    cuda.syncthreads()
 
     
 
-    res[posGx] = resc[tx] <= capacidadc[0]
+    res[posGx] = capacidad - resc[tx]
 
 
 
-@cuda.jit
+@cuda.jit('void(float32[:,:], float32[:,:], float32[:,:])', device=True)
+#@cuda.jit
 def mMult(matrizSols, pond, res):
-    solucionesc = cuda.shared.array(shape=(cuda.blockDim.x,matrizSols.shape[1]), dtype=float32)
-    pondc = cuda.shared.array(shape=(matrizSols.shape[1],1), dtype=float32)
-    resc = cuda.shared.array(shape=(cuda.blockDim.x,matrizSols.shape[1]), dtype=float32)
+    solucionesc = cuda.shared.array(shape=(BD,NUM_DIM), dtype=float32)
+    pondc = cuda.shared.array(shape=(NUM_DIM), dtype=float32)
+    resc = cuda.shared.array(shape=(BD,NUM_DIM), dtype=float32)
 
     x, y = cuda.grid(2)
     tx = cuda.threadIdx.x
@@ -76,117 +100,39 @@ def mMult(matrizSols, pond, res):
         return
 
     solucionesc[tx,ty] = matrizSols[x,y]
+#    factor = pond[y]
     pondc[ty] = pond[y]
 
     cuda.syncthreads()
 
-    resc[tx,ty] = pondc[ty] * solucionesc[tx]
+    resc[tx,ty] = (pondc[ty] * solucionesc[tx,ty])
     cuda.syncthreads()
 
     res[x,y] = resc[tx,ty]
 
-@cuda.jit
+@cuda.jit('void(float32[:,:], float32[:,:])', device=True)
 def selMayorCuda(matrizSols, res):
-    solucionesc = cuda.shared.array(shape=(cuda.blockDim.x,matrizSols.shape[1]), dtype=float32)
-    resc = cuda.shared.array(shape=(matrizSols.shape[0],1), dtype=float32)
+    solucionesc = cuda.shared.array(shape=(BD,NUM_DIM), dtype=float32)
+    resc = cuda.shared.array(shape=(NUM_FILAS,1), dtype=float32)
 
     x, y = cuda.grid(2)
     tx = cuda.threadIdx.x
     ty = cuda.threadIdx.y
+    bi = cuda.blockIdx.x
 
-    if x >= res.shape[0] or y >= res.shape[1]:
+    if x >= matrizSols.shape[0] or y >= matrizSols.shape[1]:
         print(f'fuera de rango')
         # Quit if (x, y) is outside of valid C boundary
         return
 
     solucionesc[tx,ty] = matrizSols[x,y]
-    resc[tx] = -1
+    resc[tx,bi] = -1
     cuda.syncthreads()
 
-    resc[tx][0] = solucionesc[tx,ty] if solucionesc[tx,ty] > resc[tx][0] else resc[tx][0]
+    resc[tx,bi] = solucionesc[tx,ty] if solucionesc[tx,ty] > resc[tx,bi] else resc[tx,bi]
     cuda.syncthreads()
 
-    res[x] = resc[tx]
-
-
-
-    #print('******************************************')
-    #print(cuda.grid(2))
-    #print(cuda.threadIdx.x, cuda.threadIdx.y)
-    """
-    numDim  = ponderacion.shape[0]
-    sSoluciones = cuda.shared.array(shape=(TPB,numDim), dtype=float32)
-    sResultado = cuda.shared.array(shape=(TPB,numDim), dtype=float32)
-    sPesos = cuda.shared.array(shape=(numDim), dtype=float32)
-
-    sCumple = cuda.shared.array(shape=(numDim), dtype=float32)
-    sPonderacion = cuda.shared.array(shape=(numDim), dtype=float32)
-    sPesos = cuda.shared.array(shape=(TPB, numDim), dtype=float32)
-    sSumaPesos = cuda.shared.array(shape=(TPB), dtype=float32)
-    sCapacidad = cuda.shared.array(shape=(1), dtype=float32)
-    x, y = cuda.grid(2)
-
-    tx = cuda.threadIdx.x
-    ty = cuda.threadIdx.y
-    #print(f'tx==x {tx==x}')
-    #print(f'ty==y {tx==x}')
-    #print(x)
-    #return
-    bpg = cuda.gridDim.x 
-    #print(bpg)
-    #return
-    #print(cuda.grid(2))
-    #print(ponderacion.shape)
-    #print(resultado.shape)
-    #print(bpg)
-    #return
-    if x >= resultado.shape[0] or y >= resultado.shape[1]:
-        print(f'fuera de rango')
-        # Quit if (x, y) is outside of valid C boundary
-        return
-
-    #for i in range(bpg):
-    sSoluciones[tx, y] = soluciones[x, y]
-    if x == 0:
-        sPonderacion[y] = ponderacion[y]
-        sCapacidad[0] = capacidad[0]
-
-    sPesos[tx, y] = pesos[y] * sSoluciones[tx, y]
-    #print(sSoluciones.shape)
-    #return
-    sResultado[tx,y] = sSoluciones[tx, y]*sPonderacion[y]
-    #print(f'sincronizacion 1 hilo {x}, {y}')
-    sSumaPesos[tx] += sPesos[tx, y]
-    cuda.syncthreads()
-    
-        #for j in range(numDim):
-    
-    #cuda.syncthreads()
-
-    if ty == 0:
-        #sumaPesos = np.sum(sPesos[tx])
-        cont = 0
-        while sumaPesos > sCapacidad[0]:
-            candEliminar = np.argsort(-sResultado[tx])[:5]
-            #candEliminar = sResultado.argsort()[-5:][::-1]
-            #print(f'hilo {x}, {y} candEliminar {candEliminar}')
-            idxEliminar = np.random.choice(candEliminar)
-            #print(f'hilo {x}, {y} idxEliminar {idxEliminar}')
-            sSoluciones[tx, idxEliminar] = 0
-            sPesos[tx, idxEliminar] = 0
-            sResultado[tx, idxEliminar] = 0
-            sumaPesos = np.sum(sPesos[tx])
-            cuda.syncthreads()
-            #print(f'hilo {x}, {y} sumaPesos {sumaPesos} > sCapacidad[0] {sCapacidad[0]} {sumaPesos > sCapacidad[0]}')
-    
-    #print(f'sincronizacion 2 hilo {x}, {y}')
-    cuda.syncthreads()
-    
-
-    resultado[x,y] = sSoluciones[tx, ty]
-    """
-
-
+    res[x,bi] = resc[tx,bi]
 
 
 
@@ -200,6 +146,20 @@ def aplicarPonderacion(matriz, ponderacion):
     
     tb = (tpb, clms)
     bg = int(math.ceil(fls / tb[0]))
+    mMult[bg, tb](matrizSols, pond, res)
+    return res.copy_to_host()
+
+def aplicarPesos(matriz, pesos):
+    #MULTIPLICA LA lista de soluciones por la ponderacion
+    matrizSols = cuda.to_device(matriz)
+    pond = cuda.to_device(pesos)
+    fls, clms = matriz.shape
+    tpb = 10
+    res = cuda.device_array((fls, clms))    
+    
+    tb = (tpb, clms)
+    bg = int(math.ceil(fls / tb[0]))
+#    print(numba.typeof(matrizSols))
     mMult[bg, tb](matrizSols, pond, res)
     return res.copy_to_host()
 
@@ -221,7 +181,7 @@ def selMayorFuzzy(oPonderado):
     fls, clms = oPonderado.shape
     tpb = 10
     tb = (fls,tpb)
-    bg = int( math.ceil(filas/tpb))
+    bg = int( math.ceil(fls/tpb))
     res = cuda.device_array((fls, bg))
     selMayorCuda[bg, tb](matrizSols, res)
     return res.copy_to_host()
@@ -229,19 +189,28 @@ def selMayorFuzzy(oPonderado):
 
 def obtenerIncumplidas(matriz, capacidad):
     #revisa que cada fila de la matriz cumpla
-    smatrizSols = cuda.to_device(matriz)
-    scapacidad = cuda.to_device(np.array([capacidad]))
+#    print(matriz.shape)
+    smatrizSols = cuda.to_device(matriz.astype('float32'))
+#    scapacidad = cuda.to_device(capacidad)
     fls, clms = matriz.shape
-    tpb = 100
-    res = cuda.device_array((fls, 1))    
+    tpb = 1
+    res = cuda.device_array((fls))    
     
     tb = (tpb, clms)
     bg = int(math.ceil(fls / tb[0])) if fls >= tb[0] else 1
-    cumple[bg, tb](smatrizSols, scapacidad, res)
+#    print(f'hola')
+    print(f'bg {bg}')
+    print(f'tb {tb}')
+    print(f'smatrizSols {smatrizSols.copy_to_host().shape} {smatrizSols.copy_to_host().dtype}')
+#    print(f'scapacidad {scapacidad.copy_to_host()[0]} {scapacidad.copy_to_host().dtype}')
+    print(f'res {res.copy_to_host().shape} {res.copy_to_host().dtype}')
+    print(CAPACIDAD)
+    cumple[bg, tb](smatrizSols, CAPACIDAD, res)
+#    print(f'chao')
     return res.copy_to_host()
 
 start = datetime.now()
-problema = KP(f'problemas/knapsack/instances/knapPI_1_500_1000_1')
+
 
 valores = problema.instance.itemValues
 pesos = problema.instance.itemWeights
@@ -249,9 +218,10 @@ ponderacion = 1/(valores/(pesos+1))
 capacidad = problema.instance.capacidad
 
 
-#print(ponderacion.shape)
+print(pesos.shape)
 
-origen = np.ones((100, problema.instance.numItems))
+origen = np.ones((NUM_FILAS, problema.instance.numItems))
+pesos = aplicarPesos(origen, pesos)
 incumplidas = obtenerIncumplidas(origen, capacidad)
 print(incumplidas)
 while incumplidas.size > 0:
